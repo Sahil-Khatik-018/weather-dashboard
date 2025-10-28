c5775ff9b6f29344b57e2c096de5f76e66d40bae
Sahil Khatik <sahilkhatik068@gmail.com>
2025-10-28 19:54:31 +0530

    Fix Route Error

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/api/weather/coords", async (req, res) => {
     try {
